1085e1e40af1819c5ff50f76b6dcecf12b1610fc
jesbell <joselyngonzalezma@gmail.com>
2024-02-19 17:41:45 -0300

    Agregando y arreglando comentarios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,30 @@
 // Paso 1: Solicitar datos
 var cantidad = +prompt("Ingresa la cantidad de jugadas:");
-var n = 0;
-// Jugadas 1: Piedra 2: Papel 3: Tijeras
+var n = 0; //Será nuestro contador, ira incrementando
+
+// Jugadas a elegir por el usuario:
+// 1: Piedra, 2: Papel, 3: Tijeras
+
 
 // Paso 2: operacion
-
 while(n < cantidad){
+    //Indica el número de jugada
     alert("Jugada Nº: "+ (n+1));
+    //Pidiendo al usuario que elija su jugada
     var jugador = +prompt("Ingresa su jugada. 1: Tijeras, 2: Piedra, 3: Papel \n (SOLO número del 1 al 3)");
+    
     var maquina = Math.floor(Math.random() * 3) + 1;
 
-    alert("Opción maquina: "+ maquina);
-
     if(jugador == maquina){
-        alert("Has EMPATADO!")
+        alert("¡HAS EMPATADO!")
     }
     else if (
         (jugador == 1 && maquina === 3) ||
         (jugador == 2 && maquina == 1) ||
         (jugador == 3 && maquina == 2)){
-        alert("Ganaste 😎");
+        alert("¡¡ GANASTE 😎 !!");
     } else {
-        alert("Pierdes 🙁");
+        alert("Perdiste 🙁");
     }
 
     n++;
