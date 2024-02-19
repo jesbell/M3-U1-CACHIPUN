e9a1fe9c7e2dca19bfc8727040639b774a4e372d
jesbell <joselyngonzalezma@gmail.com>
2024-02-19 17:38:48 -0300

    Agregando funcionalidad y logica al juego de cachipun

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,30 @@
 // Paso 1: Solicitar datos
+var cantidad = +prompt("Ingresa la cantidad de jugadas:");
+var n = 0;
 // Jugadas 1: Piedra 2: Papel 3: Tijeras
-var jugador = 1;
-var cantidad = 2;
-var maquina = Math.random(1, 3);
 
 // Paso 2: operacion
 
-// For // While
-if (
-  (jugador === "1 piedra" && maquina === "3 tijeras") ||
-  (jugador === "2 papel" && maquina === "1 piedra") ||
-  (jugador === "3 tijeras" && maquina === "1 papel")
-) {
-  console.log("Ganaste");
-} else {
-  console.log("Pierdes");
+while(n < cantidad){
+    alert("Jugada Nº: "+ (n+1));
+    var jugador = +prompt("Ingresa su jugada. 1: Tijeras, 2: Piedra, 3: Papel \n (SOLO número del 1 al 3)");
+    var maquina = Math.floor(Math.random() * 3) + 1;
+
+    alert("Opción maquina: "+ maquina);
+
+    if(jugador == maquina){
+        alert("Has EMPATADO!")
+    }
+    else if (
+        (jugador == 1 && maquina === 3) ||
+        (jugador == 2 && maquina == 1) ||
+        (jugador == 3 && maquina == 2)){
+        alert("Ganaste 😎");
+    } else {
+        alert("Pierdes 🙁");
+    }
+
+    n++;
 }
+
+alert("Gracias por jugar 😎")
